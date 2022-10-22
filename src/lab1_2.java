efc49695f6f10bffca89ddf651b5cfd54bfb6a1d
Jeacson_Snake <Jeacson_Snake@outlook.com>
2022-10-22 22:16:04 +0800

    实现输出“switch”结构数，并输出每组对应的“case”数量

--- src/lab1_2.java
+++ src/lab1_2.java
@@ -3,18 +3,19 @@ package src;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class lab1_2 {
-    String[] specialPatterArr = {"switch", "if", "else"};
-    boolean[] hasSpecialPattern = { false };
-    boolean[] layer = { false };
-    
+    // String[] specialPatterArr = {"switch", "if", "else"};
+    static boolean[] hasSpecialPattern = { false, false, false };
+    static boolean[] layer = { false };
+
     public static void main(String[] args) throws IOException {
         BufferedReader bReader = new BufferedReader(new FileReader("test.c"));
         String readString;
-        String patternStr = "abstract default goto switch boolean if package nchronzed break double implements private this byte else import protected throw throws case extends instanceof public transient catch int return char final interface short try class finally long static void const float native strictfp volatile continue for new super while assert enum";
+        String patternStr = "switch if else abstract default goto boolean package nchronzed break double implements private this byte import protected throw throws case extends instanceof public transient catch int return char final interface short try class finally long static void const float native strictfp volatile continue for new super while assert enum";
         String[] patternArr = patternStr.split(" ");
         StringBuffer readStringBuffer = new StringBuffer();
         int count1 = 0;
@@ -23,31 +24,55 @@ public class lab1_2 {
         }
         bReader.close();
         for (int i = 0; i < patternArr.length; i++) {
-            count1 += findByRegex(patternArr[i], readStringBuffer);
+            count1 += findByRegex(patternArr[i], readStringBuffer, i);
         }
         System.out.println(count1);
+        findSwitch(readStringBuffer);
 
     }
 
-
-    public static int findByRegex(String pattern, StringBuffer readStringBuffer) {
+    public static int findByRegex(String pattern, StringBuffer readStringBuffer, int index) {
+        boolean active = false;
+        int countNum = 0;
+        if (index <= 2) {
+            active = true;
+        }
         Pattern patternObj = Pattern.compile(pattern);
         Matcher macherObj = patternObj.matcher(readStringBuffer);
-        int countNum = 0;
         while (macherObj.find()) {
+            if (active) {
+                hasSpecialPattern[index] = true;
+                active = false;
+            }
             countNum++;
         }
         return countNum;
     }
 
-    public static int findSwitch(String pattern, StringBuffer readStringBuffer) {
-        Pattern patternObj = Pattern.compile(pattern);
+    public static void findSwitch(StringBuffer readStringBuffer) {
+        Pattern patternObj = Pattern.compile("switch.*?}");
         Matcher macherObj = patternObj.matcher(readStringBuffer);
-        int countNum = 0;
+        String subString = "";
+        Pattern subPatternObj = Pattern.compile("case");
+        Matcher subMacherObj = null;
+        int switchNum = 0;
+        int[] caseNumArr = { 0 };
         while (macherObj.find()) {
-            countNum++;
+            switchNum++;
+            caseNumArr = Arrays.copyOf(caseNumArr, switchNum);
+            subString = macherObj.toString();
+            subMacherObj = subPatternObj.matcher(subString);
+            while (subMacherObj.find()) {
+                caseNumArr[switchNum - 1]++;
+            }
+
         }
-        return countNum;
+        System.out.println("switch num: " + switchNum);
+        System.out.print("case num:");
+        for (int i = 0; i < caseNumArr.length; i++) {
+            System.out.print(" " + caseNumArr[i]);
+        }
+        System.out.println();
     }
 
     public static int findIfElse(String pattern, StringBuffer readStringBuffer) {
@@ -59,6 +84,5 @@ public class lab1_2 {
         }
         return countNum;
     }
-    
 
 }
