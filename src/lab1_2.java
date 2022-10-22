20f0681a0a5fa6cba0dc677ac03ea578f0dc0dd5
Jeacson_Snake <Jeacson_Snake@outlook.com>
2022-10-23 00:49:04 +0800

    尝试使用递归实现识别等级3、4（失败）

--- src/lab1_2.java
+++ src/lab1_2.java
@@ -12,6 +12,8 @@ public class lab1_2 {
     // String[] specialPatterArr = {"switch", "if", "else"};
     static boolean[] hasSpecialPattern = { false, false, false };
     static boolean[] layer = { false };
+    static int ifElseNum = 0;
+    static int ifElseifElseNum = 0;
 
     public static void main(String[] args) throws IOException {
         System.out.println("Please enter the file name and the level of requirement: (seprated by space)");
@@ -38,7 +40,7 @@ public class lab1_2 {
             if (level > 1) {
                 findSwitch(readStringBuffer);
                 if (level > 2) {
-                    
+                    findIE(readStringBuffer, level);
                 }
             }
 
@@ -92,14 +94,63 @@ public class lab1_2 {
         System.out.println();
     }
 
-    public static int findIfElse(String pattern, StringBuffer readStringBuffer) {
-        Pattern patternObj = Pattern.compile(pattern);
-        Matcher macherObj = patternObj.matcher(readStringBuffer);
-        int countNum = 0;
-        while (macherObj.find()) {
-            countNum++;
+    public static void findIE(StringBuffer readStringBuffer, int level) {
+        findIfElse(readStringBuffer, level);
+        System.out.println("if-else num: " + ifElseNum);
+        if (level == 4) {
+            System.out.println("if-elseif-else num: " + ifElseifElseNum);
         }
-        return countNum;
     }
 
+    public static void findIfElse(StringBuffer readStringBuffer, int level) {
+        Pattern patternObj = Pattern.compile("if.*?{");
+        Pattern subElsePatternObj = Pattern.compile("else.*?{");
+
+        Matcher macherObj = patternObj.matcher(readStringBuffer);
+        Matcher subElseMacherObj = subElsePatternObj.matcher(readStringBuffer);
+
+        String subString = "";
+        StringBuffer subStringBuffer = new StringBuffer();
+        while (macherObj.find()) {
+            subString = macherObj.toString();
+            subStringBuffer.append(subString);
+            findIfElse(readStringBuffer, level);
+
+            if (subElseMacherObj.find(macherObj.end())) {
+                subString = subElseMacherObj.toString();
+                subStringBuffer.append(subString);
+                findIfElse(readStringBuffer, level);
+                if (subElseMacherObj.start() == macherObj.end()) {
+                    ifElseNum++;
+                }
+
+            }
+
+            if (level == 4) {
+                findIfElseIf(readStringBuffer, macherObj);
+            }
+        }
+    }
+
+    public static void findIfElseIf(StringBuffer readStringBuffer, Matcher macherObj) {
+        Pattern subElseIfPatternObj = Pattern.compile("else if.*?}");
+        Pattern subElsePatternObj = Pattern.compile("else.*?}");
+
+        Matcher subElseIfMacherObj = subElseIfPatternObj.matcher(readStringBuffer);
+        Matcher subElseMacherObj = subElsePatternObj.matcher(readStringBuffer);
+
+        String subString = "";
+        StringBuffer subStringBuffer = new StringBuffer();
+
+        while (subElseIfMacherObj.find(macherObj.end())) {
+            subString = subElseIfMacherObj.toString();
+            subStringBuffer.append(subString);
+            findIfElse(readStringBuffer, 4);
+        }
+        if (subElseMacherObj.find(macherObj.end())) {
+            if (subElseMacherObj.start() == subElseIfMacherObj.end()) {
+                ifElseifElseNum++;
+            }
+        }
+    }
 }
